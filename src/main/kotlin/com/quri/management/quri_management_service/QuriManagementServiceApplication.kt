8d0a1d92bc90b081943c2fb4a58ef3f3e0b4365c
Kevin Lizarazu <sp1cybo1@vt.edu>
2025-10-28 16:56:09 -0400

    Initial SpringBoot Kotlin setup

---
+++ src/main/kotlin/com/quri/management/quri_management_service/QuriManagementServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.quri.management.quri_management_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuriManagementServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuriManagementServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/quri/management/quri_management_service/QuriManagementServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.quri.management.quri_management_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuriManagementServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
