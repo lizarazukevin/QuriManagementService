4d84746215c8c04830d37733b1fd9960d275c948
Kevin Lizarazu <sp1cybo1@vt.edu>
2026-01-15 19:30:02 -0500

    [FIX] Gradle test dependencies and specified Java21 in project

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
 kotlin {
