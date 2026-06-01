f5c8f1318e7d96246a05e7f7009522f47d275b69
Kevin Lizarazu <sp1cybo1@vt.edu>
2026-05-31 20:36:49 -0400

    [TEST](billFieldsValidator): add coverage for conditional updates on name and description

---
+++ src/test/kotlin/com/quri/management/api/validators/bill/BillFieldsValidatorTest.kt
@@ -0,0 +1,40 @@
+package com.quri.management.api.validators.bill
+
+import com.quri.client.model.MonetaryAmount
+import com.quri.management.api.validation.bill.BillFieldsValidator
+import com.quri.management.api.validation.model.MonetaryAmountValidator
+import io.kotest.core.spec.style.DescribeSpec
+import org.bson.types.ObjectId
+import java.math.BigDecimal
+
+@Suppress("unused")
+class BillFieldsValidatorTest :
+    DescribeSpec({
+
+        val monetaryAmountValidator = MonetaryAmountValidator()
+        val validator = BillFieldsValidator(monetaryAmountValidator)
+
+        describe("validate on shared bill properties") {
+
+            context("when all fields are null") {
+                it("passes") {
+                    validator.validate("field")
+                }
+            }
+
+            context("when all valid fields are present") {
+                it("passes") {
+                    validator.validate(
+                        "field",
+                        name = "test name",
+                        description = "test description",
+                        balance = MonetaryAmount.builder()
+                            .amount(BigDecimal(100))
+                            .currency("USD")
+                            .build(),
+                        receipts = listOf(ObjectId().toString()),
+                    )
+                }
+            }
+        }
+    })
