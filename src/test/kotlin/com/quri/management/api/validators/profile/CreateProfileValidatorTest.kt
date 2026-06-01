5c248652bff33cbf9020fedf947faf48b671d7b1
Kevin Lizarazu <sp1cybo1@vt.edu>
2026-05-31 21:54:35 -0400

    [TEST](createProfileValidator): add coverage for thin layer

---
+++ src/test/kotlin/com/quri/management/api/validators/profile/CreateProfileValidatorTest.kt
@@ -0,0 +1,28 @@
+package com.quri.management.api.validators.profile
+
+import com.quri.management.api.validation.model.AddressFieldsValidator
+import com.quri.management.api.validation.model.UserLocationValidator
+import com.quri.management.api.validation.profile.CreateProfileValidator
+import com.quri.management.api.validation.profile.ProfileFieldsValidator
+import com.quri.management.fixtures.models.ProfileFixtures
+import io.kotest.core.spec.style.DescribeSpec
+
+@Suppress("unused")
+class CreateProfileValidatorTest :
+    DescribeSpec({
+
+        val addressFieldsValidator = AddressFieldsValidator()
+        val userLocationValidator = UserLocationValidator(addressFieldsValidator)
+        val profileFieldsValidator = ProfileFieldsValidator(userLocationValidator)
+        val validator = CreateProfileValidator(profileFieldsValidator)
+
+        describe("validate") {
+
+            context("when all required fields are valid") {
+                it("passes") {
+                    val input = ProfileFixtures.aCreateProfileInput()
+                    validator.validate("createProfile", input)
+                }
+            }
+        }
+    })
